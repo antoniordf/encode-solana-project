5fd9f282fdc449cca33af48ebdb7b00950fde8d6
simon <simon@clearfly.co.uk>
2024-01-16 14:55:35 +0000

    Fix IDL missing type during anchor test. Seems BorchSerialise can't handle structs which contain accounts.

--- blockchain/programs/blocktowin/src/states/competition_user.rs
+++ blockchain/programs/blocktowin/src/states/competition_user.rs
@@ -1,7 +1,6 @@
 use anchor_lang::prelude::*;
 
-#[account]
-#[derive(Default)]
+#[derive(AnchorSerialize, AnchorDeserialize, Default, Clone, Copy)]
 pub struct CompetitionUser {
 
     /// Competition users public address - 32
@@ -11,9 +10,11 @@ pub struct CompetitionUser {
     pub tickets: i64
 }
 
+/*
 impl CompetitionUser {
     /// Calculate storage size based on struct defined above
     pub fn storage_size(&self) -> usize {
         std::mem::size_of::<CompetitionUser>() as usize
     }
 }
+*/
