8f72d5a41583c2fa0dc17f7305f17712efda95cb
Roy Scheeren <hi@royscheeren.com>
2024-01-16 15:43:45 +0100

    feat(frontend): add wallet connect

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { Providers } from './providers'
+import { Providers } from './common/providers'
 
 import './globals.css'
 
--- frontend/app/modules/NavBar/NavBar.tsx
+++ frontend/app/modules/NavBar/NavBar.tsx
@@ -1,19 +1,30 @@
+'use client'
+
 import { Stack } from '@chakra-ui/react'
 import React, { FC } from 'react'
+import { useWallet } from '@solana/wallet-adapter-react'
+import {
+  WalletMultiButton
+} from '@solana/wallet-adapter-react-ui'
+
 import { NavigationItem } from '../NavigationItem'
 
+
 export const NavBar:FC = () => {
-   return (
-    <div className='bg-slate-950 w-full flex justify-center items-center'>
-      <div className='max-w-5xl w-full py-6 flex justify-between'>
-        <div className='font-bold text-2xl'>Block2Win</div>
-        <div>
-          <Stack direction='row' spacing={4}>
-            <NavigationItem to='/'>Home</NavigationItem>
-            <NavigationItem to='/competitions'>Competitions</NavigationItem>
-          </Stack>
-        </div>
+  const { publicKey, sendTransaction } = useWallet()
+  
+  return (
+  <div className='bg-slate-950 w-full flex justify-center items-center'>
+    <div className='max-w-5xl w-full py-6 flex justify-between items-center'>
+      <div className='font-bold text-2xl'>Block2Win</div>
+      <div>
+        <Stack direction='row' spacing={4} className='items-center'>
+          <NavigationItem to='/'>Home</NavigationItem>
+          <NavigationItem to='/competitions'>Competitions</NavigationItem>
+          <WalletMultiButton />
+        </Stack>
       </div>
     </div>
-   )
+  </div>
+  )
 }
