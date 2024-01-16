d21f53c0b5ea4fb85048b89559b438b666ae1f5f
Antonio Furtado <antoniordf@hotmail.com>
2024-01-16 15:14:21 +0000

    We did our best

--- blockchain/programs/block2win/src/instructions/buy_tickets.rs
+++ blockchain/programs/block2win/src/instructions/buy_tickets.rs
@@ -1,16 +1,50 @@
+use crate::error::ErrorCodes;
+use crate::states::CompetitionModel;
+use crate::CompetitionUser;
 use anchor_lang::prelude::*;
 
 #[derive(Accounts)]
 #[instruction()]
-pub struct BuyTickets{
-
+pub struct BuyTickets<'info> {
     // Accounts needed to buy tickets
+    #[account(mut)]
+    pub buyer: Signer<'info>,
 
+    #[account(mut)]
+    pub competition: Account<'info, CompetitionModel>,
+
+    pub system_program: Program<'info, System>,
+
+    #[account(mut)]
+    pub user_competition_tickets: Account<'info, CompetitionUser>,
 }
 
+// Why is number u16 here and u64 in the state?
 pub fn handler(ctx: Context<BuyTickets>, account: Pubkey, number: u16) -> Result<()> {
-
     // Logic for handling purchase of tickets
+    let competition = &mut ctx.accounts.competition;
+
+    let clock = Clock::get()?;
+    let current_timestamp = clock.unix_timestamp;
+
+    if current_timestamp > competition.closedate || current_timestamp < competition.opendate {
+        return Err(ErrorCodes::NotAllowed.into());
+    }
+
+    if competition.soldtickets + number as u64 > competition.totaltickets {
+        return Err(ErrorCodes::MaxEntries.into());
+    }
+
+    // Do we need to create a PDA for this buyer/competition if one doesnt exist?
+
+    // Check if user already has entries
+    let user_competition_tickets = &mut ctx.accounts.user_competition_tickets;
+    // TODO check if this is correct
+    if user_competition_tickets.tickets > competition.maxentries as i64 {
+        return Err(ErrorCodes::AlreadyEntered.into());
+    }
+
+    // TODO check if user has enough funds
 
     Ok(())
 }
--- blockchain/programs/block2win/src/instructions/select_winner.rs
+++ blockchain/programs/block2win/src/instructions/select_winner.rs
@@ -1,16 +1,40 @@
+use crate::error::ErrorCodes;
+use crate::states::CompetitionModel;
 use anchor_lang::prelude::*;
 
 #[derive(Accounts)]
 #[instruction()]
-pub struct SelectWinner {
-
+pub struct SelectWinner<'info> {
     // Accounts needed to process selection
+    #[account(mut)]
+    pub competition: Account<'info, CompetitionModel>,
 
+    #[account(mut)]
+    pub authority: Signer<'info>, // TODO admin contract owner check??
+
+    pub system_program: Program<'info, System>,
 }
 
-pub fn handler(ctx: Context<SelectWinner>) -> Result<()> {
-
+pub fn handler(ctx: Context<SelectWinner>) -> Result<Pubkey> {
     // Logic for handling selection of winner
+    // Flatten the entries array into a new vector where each user appears a number of times equal to their number of tickets
+    let mut weighted_entries = Vec::new();
+    for entry_option in ctx.accounts.competition.entries.iter() {
+        if let Some(entry) = entry_option {
+            for _ in 0..entry.tickets {
+                weighted_entries.push(entry);
+            }
+        }
+    }
 
-    Ok(())
+    // Get the current block's timestamp
+    let timestamp = Clock::get()?.unix_timestamp;
+
+    // Use the timestamp as a seed to generate a pseudo-random number
+    let random_number = timestamp as usize % weighted_entries.len();
+
+    // Select the winner
+    let winner = weighted_entries[random_number].authority;
+
+    Ok(winner)
 }
