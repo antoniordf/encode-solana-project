b6fc98da3016fba703c113a18d3e8c8ab07de651
Roy Scheeren <hi@royscheeren.com>
2024-01-17 15:29:31 +0100

    feat(frontend): add create competition skeleton
    Signed-off-by: Roy Scheeren <hi@royscheeren.com>

--- frontend/app/modules/AddCompetition/AddCompetition.tsx
+++ frontend/app/modules/AddCompetition/AddCompetition.tsx
@@ -1,13 +1,53 @@
-import { Button } from '@chakra-ui/react'
+'use client'
+
+import { Button, Input, Textarea } from '@chakra-ui/react'
 import React, { FC } from 'react'
 
 export const AddCompetition:FC = () => {
+  const [ name, setName ] = React.useState<string>('')
+  const [ description, setDescription ] = React.useState<string>('')
+  const [ loading, setLoading ] = React.useState<boolean>(false)
+  const [ error, setError ] = React.useState<string>('')
+
+  const onChangeName = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value)
+  }
+
+  const onChangeDescription = (e:React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDescription(e.target.value)
+  }
+
+  const addCompetition = async () => {
+    if (!name) {
+      setError('Name is required')
+      return
+    }
+
+    if (!description) {
+      setError('Description is required')
+      return
+    }
+
+    setError('')
+    console.log('Adding Competition', name, description)
+  }
 
   return (
     <div>
-      <Button colorScheme="purple">Add Competition</Button>
-      <div>
-        
+      <div className='max-w-xs'>
+        <h2 className='font-bold text-2xl mb-2'>Add Competition</h2>
+        <div className='mb-4'>  
+          <Input placeholder='Name' onChange={onChangeName} />
+        </div>
+        <div className='mb-4'>
+          <Textarea placeholder='Description' onChange={onChangeDescription} />
+        </div>
+        {error ? 
+          <div className='mb-4'>
+            <p className='text-red-500'>{error}</p>
+          </div> : null
+        } 
+        <Button colorScheme="purple" onClick={addCompetition}>Add Competition</Button>
       </div>
     </div>
   )
