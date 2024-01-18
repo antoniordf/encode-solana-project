1e82ae07d8cd8072e7333526209653e4441797c6
Roy Scheeren <hi@royscheeren.com>
2024-01-18 13:30:18 +0100

    feat(frontend): update general styling
    Signed-off-by: Roy Scheeren <hi@royscheeren.com>

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en" style={{backgroundColor: '#070709'}}>
+      <body className={`${inter.className} text-white `}>
         <Providers>{children}</Providers>
       </body>
     </html>
